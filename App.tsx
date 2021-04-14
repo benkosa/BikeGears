d0c25db6dead2e1efc4916870b27d1e1d3c6e948
f <f>
2021-04-14 20:31:44 +0200

    firebase auth

--- App.tsx
+++ App.tsx
@@ -1,11 +1,36 @@
-import { StatusBar } from "expo-status-bar";
+
+// @refresh state
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, Image, Button } from "react-native";
 import LandingScreen from "./app/screens/LandingScreen";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { LogBox } from 'react-native';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
 import LoginScreen from "./app/screens/LoginScreen";
 import SavedScreen from "./app/screens/SavedScreen";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDk-p75QX33j-G0Kj_p-CoeX7hTNV-_96I",
+  authDomain: "bikegears-282f0.firebaseapp.com",
+  projectId: "bikegears-282f0",
+  storageBucket: "bikegears-282f0.appspot.com",
+  messagingSenderId: "1098564175858",
+  appId: "1:1098564175858:web:29b091130ab07537880d6b",
+  measurementId: "G-F7B983RST0"
+};
+// Initialize Firebase
+if(firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+  //firebase.analytics();
+}
+
+
+
+LogBox.ignoreLogs(['Setting a timer for a long period of time']);
+
 const Tab = createBottomTabNavigator();
 const TabNavigator = () => (
   <Tab.Navigator>
--- app/screens/SavedScreen.tsx
+++ app/screens/SavedScreen.tsx
@@ -1,15 +1,58 @@
-import React from "react";
+
+import React, { Component } from "react";
 import { SafeAreaView, Text, StyleSheet } from "react-native";
 
-function SavedScreen() {
-  const handlePress = () => console.log("test");
-  return (
-    <SafeAreaView>
-      <Text numberOfLines={2} onPress={handlePress}>
-        SavedScreen
-      </Text>
-    </SafeAreaView>
-  );
+import firebase from 'firebase';
+
+class SavedScreen extends Component {
+
+  constructor(props:any) {
+    super(props);
+    this.state = {
+      isLogged: true
+    };
+  }
+
+  /**
+   * vola sa pri vytvoreni componentu
+   */
+  componentDidMount() {
+    this.checkIsLogged();
+  }
+
+  componentWillUnmount() {
+    this.firebaseUnsubscribe();
+  }
+
+  firebaseUnsubscribe:firebase.Unsubscribe = () => {};
+
+  /**
+   * kontrola ci je uzivatel prihlaseny
+   */
+  checkIsLogged = () => {
+    this.firebaseUnsubscribe = firebase.auth().onAuthStateChanged((user) =>{
+      
+      if(user) {
+        this.setState({isLogged: true});
+        console.log("prihlaseny");
+      } else {
+        this.setState({isLogged: false});
+        console.log("neprihlaseny");
+      }
+    })
+  }
+
+  render() {
+    console.log("render");
+    return (
+      <SafeAreaView>
+        <Text numberOfLines={2}>
+          {/*@ts-ignore*/}
+          {this.state.isLogged ? 'prihlaseny' : 'neprihlaseny'}
+        </Text>
+      </SafeAreaView>
+    );
+  }
 }
 
 export default SavedScreen;
