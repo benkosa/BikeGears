02e5963e527d721d6f69fbad8c97aa60203e5af5
f <f>
2021-04-16 21:33:09 +0200

    landig

--- app/screens/LandingScreen/LandingScreen.tsx
+++ app/screens/LandingScreen/LandingScreen.tsx
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
-import { SafeAreaView, View, ScrollView } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  ScrollView,
+  Button,
+  TouchableWithoutFeedback,
+  Text,
+} from "react-native";
 
-import GearsRatioTable from '../../components/GearsRatioTable/GearsRatoTable';
+import GearsRatioTable from "../../components/GearsRatioTable/GearsRatoTable";
 import styles from "./LandingScreen-style";
+import { Picker } from "@react-native-picker/picker";
+import RNPickerSelect from "react-native-picker-select";
 
 class LandingScreen extends Component {
   constructor(props: any) {
@@ -14,15 +23,92 @@ class LandingScreen extends Component {
     };
   }
 
+  placeholder = {
+    label: "Select a sport...",
+    value: null,
+    color: "#9EA0A4",
+  };
+
+  InputAccessoryView() {
+    return (
+      <View>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            this.setState(
+              {
+                //favSport5: this.state.previousFavSport5,
+              },
+              () => {
+                //this.inputRefs.favSport5.togglePicker(true);
+              }
+            );
+          }}
+          hitSlop={{ top: 4, right: 4, bottom: 4, left: 4 }}
+        >
+          <View testID="needed_for_touchable">
+            <Text style={[{ fontWeight: "normal", color: "red" }]}>Cancel</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <Text>Name | Prefer</Text>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            //this.inputRefs.favSport5.togglePicker(true);
+          }}
+          hitSlop={{ top: 4, right: 4, bottom: 4, left: 4 }}
+        >
+          <View testID="needed_for_touchable">
+            <Text>Done</Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    );
+  }
+
   render() {
     const state = this.state;
     return (
       <SafeAreaView>
         <ScrollView>
           <View style={styles.menu}>
-            <View style={styles.menuItem}></View>
-            <View style={styles.menuItem}></View>
-            <View style={styles.menuItem}></View>
+            <View style={styles.menuItem}>
+              <RNPickerSelect
+                onValueChange={(value) => console.log(value)}
+                placeholder={{ label: "Select wheel size", color: "#9EA0A4" }}
+                items={wheelSize}
+              />
+            </View>
+            <View style={styles.menuItem}>
+              <RNPickerSelect
+                onValueChange={(value) => console.log(value)}
+                placeholder={{ label: "Select crank size", color: "#9EA0A4" }}
+                items={crankSize}
+              />
+              <RNPickerSelect
+                onValueChange={(value) => console.log(value)}
+                placeholder={{ label: "Select crank type", color: "#9EA0A4" }}
+                items={[
+                  { label: "26", value: "26" },
+                  { label: "27.5", value: "27" },
+                  { label: "29", value: "29" },
+                ]}
+              />
+            </View>
+            <View style={styles.menuItem}>
+            <RNPickerSelect
+                onValueChange={(value) => console.log(value)}
+                placeholder={{ label: "Select casete size", color: "#9EA0A4" }}
+                items={caseteSize}
+              />
+              <RNPickerSelect
+                onValueChange={(value) => console.log(value)}
+                placeholder={{ label: "Select casete type", color: "#9EA0A4" }}
+                items={[
+                  { label: "26", value: "26" },
+                  { label: "27.5", value: "27" },
+                  { label: "29", value: "29" },
+                ]}
+              />
+            </View>
           </View>
           <GearsRatioTable
             wheelSize={1.04}
@@ -32,6 +118,7 @@ class LandingScreen extends Component {
             casete={state.casete}
           ></GearsRatioTable>
           <View style={styles.saveBtn}>
+            <Button title="Save" onPress={()=>console.log("save")}></Button>
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -40,3 +127,27 @@ class LandingScreen extends Component {
 }
 
 export default LandingScreen;
+
+const wheelSize = [
+  { label: "26", value: "26" },
+  { label: "27.5", value: "27" },
+  { label: "29", value: "29" },
+  { label: "28", value: "28" },
+  { label: "20", value: "20" },
+  { label: "24", value: "24" },
+];
+
+const crankSize = [
+  { label: "1", value: "1" },
+  { label: "2", value: "2" },
+  { label: "3", value: "3" },
+];
+
+let caseteSize:any = [];
+for (let i = 1; i <= 13; i++) {
+  caseteSize.push({ label: i+"", value: i+"" })
+}
+
+interface PickerItem {
+  label: string, value: string
+}
