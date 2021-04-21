90a0f5f302e95c50067691c8c8f59aa6c7f7e590
f <f>
2021-04-21 13:41:11 +0200

    deletion

--- app/store/count.tsx
+++
@@ -1,5 +0,0 @@
-import GlobalStore from 'react-native-global-state-hooks';
-
-const countStore = new GlobalStore(0);
-
-export const useCount = countStore.getHook();
--- app/store/globalStore.tsx
+++
@@ -1,5 +0,0 @@
-import GlobalStore from 'react-native-global-state-hooks';
-
-const gStore = new GlobalStore(false)
-
-export const useGStore = gStore.getHook();
