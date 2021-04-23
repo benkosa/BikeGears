51dc7bb0bf1b21f304b089375368722d0f1987e6
f <f>
2021-04-23 10:08:30 +0200

    nastavniea

---
+++ app/screens/SettingsScreen/SettingsScreen-lang.tsx
@@ -0,0 +1,30 @@
+const en = {
+    SIGN_IN: "Sign in with google",
+    SIGN_OUT: "Sign out",
+
+    LANG_BTN: ["Slovak", "English"],
+    LANG_BTN_TITLE: "Language: ",
+
+    HOMESCREEN_BTN: ["Settings", "Home", "Saved"],
+    HOMESCREEN_BTN_TITLE: "Home screen: ",
+
+    APIRENCE_BTN: ["Light", "Dark"],
+    APIRENCE_BTN_TITLE: "App apirence: ",
+}
+
+const sk = {
+    SIGN_IN: "Prihlásiť sa cez Google",
+    SIGN_OUT: "Odhlásiť sa",
+
+    LANG_BTN: ["Slovenský", "Anglický"],
+    LANG_BTN_TITLE: "Jazyk: ",
+
+    HOMESCREEN_BTN: ["Nastavenia", "Domov", "Uložené"],
+    HOMESCREEN_BTN_TITLE: "Domovská obrazovka: ",
+
+    APIRENCE_BTN: ["Svetlý", "Tmavý"],
+    APIRENCE_BTN_TITLE: "Vzhľad aplikácie: ",
+    
+}
+
+export default {sk, en};
--- app/screens/SettingsScreen/SettingsScreen.tsx
+++ app/screens/SettingsScreen/SettingsScreen.tsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
-import { SafeAreaView, Button } from "react-native";
+import { SafeAreaView, View } from "react-native";
 import * as firebase from "firebase";
 import LoginButton from "../../components/LoginButton/LoginButton";
 
-import { connect } from 'react-redux';
+import language from "./SettingsScreen-lang";
+
+import { connect } from "react-redux";
+import { ButtonGroup, Button, Text, ListItem } from "react-native-elements";
 
 /**
  * oprazovka nastaveni
@@ -13,14 +16,29 @@ class SettingsScreen extends Component {
     super(props);
     this.state = {
       isLogged: true,
+      selectedLanguage: 0,
+      selectedHomeScreen: 1,
+      selectedApirence: 0,
     };
     this.getProfile();
   }
 
+  updateLanguage(selectedLanguage: number) {
+    this.setState({ selectedLanguage });
+  }
+
+  updateHomeScreen(selectedHomeScreen: number) {
+    this.setState({ selectedHomeScreen });
+  }
+
+  updateApirence(selectedApirence: number) {
+    this.setState({ selectedApirence });
+  }
+
   getProfile = () => {
-    console.log("bol som tu")
+    console.log("bol som tu");
     const token = firebase.auth().currentUser?.uid;
-    console.log(token)
+    console.log(token);
     if (token) {
       firebase
         .firestore()
@@ -32,8 +50,6 @@ class SettingsScreen extends Component {
         });
     }
   };
-  
-  
 
   componentDidMount() {
     this.firebaseUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
@@ -48,21 +64,61 @@ class SettingsScreen extends Component {
   firebaseUnsubscribe: firebase.Unsubscribe = () => {};
 
   render() {
-    console.log(this.props.global.user);
     return (
       <SafeAreaView>
-        {!this.state.isLogged && <LoginButton title="Sign in with google" />}
+        {!this.state.isLogged && <LoginButton title={language["sk"].SIGN_IN} />}
+        
+
+        <ListItem bottomDivider topDivider>
+          <ListItem.Content>
+            <ListItem.Title>{language["sk"].LANG_BTN_TITLE}</ListItem.Title>
+            <ButtonGroup
+            onPress={(value) => this.updateLanguage(value)}
+            selectedIndex={this.state.selectedLanguage}
+            buttons={language["sk"].LANG_BTN}
+            containerStyle={{}}
+          />
+          </ListItem.Content>
+        </ListItem>
+
+        <ListItem bottomDivider>
+          <ListItem.Content>
+            <ListItem.Title>{language["sk"].HOMESCREEN_BTN_TITLE}</ListItem.Title>
+            <ButtonGroup
+            onPress={(value) => this.updateHomeScreen(value)}
+            selectedIndex={this.state.selectedHomeScreen}
+            buttons={language["sk"].HOMESCREEN_BTN}
+            containerStyle={{}}
+          />
+          </ListItem.Content>
+        </ListItem>
+
+        <ListItem bottomDivider>
+          <ListItem.Content>
+            <ListItem.Title>{language["sk"].APIRENCE_BTN_TITLE}</ListItem.Title>
+            <ButtonGroup
+            onPress={(value) => this.updateApirence(value)}
+            selectedIndex={this.state.selectedApirence}
+            buttons={language["sk"].APIRENCE_BTN}
+            containerStyle={{}}
+          />
+          </ListItem.Content>
+        </ListItem>
+
         {this.state.isLogged && (
-          <Button title="Sign out" onPress={() => firebase.auth().signOut()} />
+          <Button
+            title={language["sk"].SIGN_OUT}
+            onPress={() => firebase.auth().signOut()}
+          />
         )}
       </SafeAreaView>
     );
   }
 }
 
-const mapStateToProps = (state: { global: any; }) => {
-  const { global } = state
-  return { global }
+const mapStateToProps = (state: { global: any }) => {
+  const { global } = state;
+  return { global };
 };
 
 export default connect(mapStateToProps)(SettingsScreen);
