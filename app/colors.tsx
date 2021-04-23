099b4e8bce175ed591406c5b6f726c3c0c8c545d
f <f>
2021-04-23 14:57:15 +0200

    dokonecny profile

---
+++ app/colors.tsx
@@ -0,0 +1,11 @@
+const dark = {
+    primary: "black",
+    primary_shade: "#212121",
+
+    secondary: "#b030b0",
+    secondary_shade: "#602080",
+
+    light: "#f5f7fa",
+}
+
+export default dark;
