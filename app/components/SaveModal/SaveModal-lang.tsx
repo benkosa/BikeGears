222b6abdfe9552a5b0cc61fadf5efd1b9b30e170
f <f>
2021-04-26 17:37:57 +0200

    modal prirpava pred dark mode a jazyk

---
+++ app/components/SaveModal/SaveModal-lang.tsx
@@ -0,0 +1,15 @@
+interface SaveModalLang {
+}
+
+const en: SaveModalLang = {
+};
+
+const sk: SaveModalLang = {
+};
+
+const language: { [key: string]: SaveModalLang } = {
+    "sk": sk,
+    "en": en
+}
+
+export default language;
