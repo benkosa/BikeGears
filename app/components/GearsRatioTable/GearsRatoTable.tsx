1cd9564e27c374d5f724e80a98eb555e7739f397
f <f>
2021-05-15 19:44:32 +0200

    fix update state on gear table

--- app/components/GearsRatioTable/GearsRatoTable.tsx
+++ app/components/GearsRatioTable/GearsRatoTable.tsx
@@ -49,47 +49,51 @@ class GearsRatioTable extends Component<GearsRatioTableProps> {
     return gearRatio;
   };
 
-  gearRatio = this.calculateGearsRatio(this.props.wheelSize, this.props.crank, this.props.cassette);
-  max = this.gearRatio[0][this.gearRatio[0].length - 1];
-  min = this.gearRatio[this.gearRatio.length - 1][0];
-
-  /**
-   * pripravene date pre tabulku
-   */
-  table: GearsRatioTableData = {
-    tableHead: [""].concat(this.props.crank.split("-")),
-    tableTitle: this.props.cassette.split("-"),
-    tableData: this.gearRatio,
-
-    table2Title: ["Max", "Min"],
-    table2Data: [[this.max], [this.min]],
-  };
-
-  
-
   render() {
+    const gearRatio = this.calculateGearsRatio(
+      this.props.wheelSize,
+      this.props.crank,
+      this.props.cassette
+    );
+    const max = gearRatio[0][gearRatio[0].length - 1];
+    const min = gearRatio[gearRatio.length - 1][0];
+
+    /**
+     * pripravene date pre tabulku
+     */
+    const table: GearsRatioTableData = {
+      tableHead: [""].concat(this.props.crank.split("-")),
+      tableTitle: this.props.cassette.split("-"),
+      tableData: gearRatio,
+
+      table2Title: ["Max", "Min"],
+      table2Data: [[max], [min]],
+    };
+
     const styleId = +this.props.global.selectedApirence;
     const style = styles[styleId];
+
+    console.log("table");
 
     return (
       <View style={style.mainContainer}>
         <View style={style.container}>
           <Table borderStyle={{ borderWidth: 1 }}>
             <Row
-              data={this.table.tableHead}
+              data={table.tableHead}
               flexArr={[1, 2, 1, 1]}
               style={style.head}
               textStyle={style.textBold}
             />
             <TableWrapper style={style.wrapper}>
               <Col
-                data={this.table.tableTitle}
+                data={table.tableTitle}
                 style={style.title}
                 heightArr={[28, 28]}
                 textStyle={style.textBold}
               />
               <Rows
-                data={this.table.tableData}
+                data={table.tableData}
                 flexArr={[2, 1, 1]}
                 style={style.row}
                 textStyle={style.text}
@@ -101,13 +105,13 @@ class GearsRatioTable extends Component<GearsRatioTableProps> {
           <Table borderStyle={{ borderWidth: 1 }}>
             <TableWrapper style={style.wrapper}>
               <Col
-                data={this.table.table2Title}
+                data={table.table2Title}
                 style={style.title}
                 textStyle={style.textBold}
                 heightArr={[28, 28]}
               />
               <Rows
-                data={this.table.table2Data}
+                data={table.table2Data}
                 flexArr={[2, 1, 1]}
                 style={style.row}
                 textStyle={style.text}
